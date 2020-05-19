689a4c4a94ad1deae640dfa93fd3e4c9c50aa53a
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-05-19 18:03:28 +0200

    first commit

---
+++ sedoo-wppl-blocklist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Sedoo - Blocklist
+ * Description: Liste de blocks
+ * Version: 0.0.1
+ * Author: Nicolas Gruwe & Pierre Vert - SEDOO DATA CENTER
+ * Author URI:      https://www.sedoo.fr 
+ * GitHub Plugin URI: sedoo/sedoo-wppl-blocklist
+ * GitHub Branch:     master
+ */
+// http://localhost/usineasite/wordpress/get/wp-admin/edit.php?post_type=wp_block
+
+include 'sedoo-wppl-blocklist-functions.php';
+include 'sedoo-wppl-blocklist-acf.php';
+include 'inc/sedoo-wppl-blocklist-acf-fields.php';
